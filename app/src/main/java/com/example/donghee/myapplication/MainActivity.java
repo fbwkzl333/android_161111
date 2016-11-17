1393ff26272e1b8122bdda173a9fc926288e93b0
fbwkzl333 <fbwkzl111@naver.com>
2016-11-17 14:10:02 +0900

    update app

--- app/src/main/java/com/example/donghee/myapplication/MainActivity.java
+++ app/src/main/java/com/example/donghee/myapplication/MainActivity.java
@@ -1,18 +1,19 @@
 package com.example.donghee.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.RatingBar;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    Button b1;
-    Button b2;
-    CheckBox C1;
-
+    Button b1, b2;
+    RatingBar r1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,37 +24,38 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getApplicationContext(),
-                        "안녕?", Toast.LENGTH_SHORT).show();
+                        "Hello This is my first application !\nThanck you very much!", Toast.LENGTH_SHORT).show();
+            }
+        });
+         b2 = (Button)findViewById(R.id.button4);
+
+        r1 = (RatingBar) findViewById(R.id.ratingBar);
+        r1.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                Toast.makeText(getApplicationContext(), (int)rating+"점 선택 해주셨습니다 ! ", Toast.LENGTH_SHORT).show();
             }
         });
 
-        b2 = (Button) findViewById(R.id.button4);
-        b2.setOnClickListener(new OnClickListener() {
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),
-                        "이름은 무엇입니까?", Toast.LENGTH_SHORT).show();
-            }
-        });
 
-
-        findViewById(R.id.checkBox).setOnClickListener(new Button.OnClickListener() {
-            public void onClick(View v) {
-                Checked(v); // 체크되었을 때 동작코드
-            }
-        });
     }
-
-
-    public String Checked(View v){
-        C1 = (CheckBox)findViewById(R.id.checkBox);
-
-        String resultText = "";
-        if (C1.isChecked()){
-            resultText = "체크했습니다";
+    public void onClick01(View v) {
+        CheckBox ch1 = (CheckBox)findViewById(R.id.checkBox);
+        if(ch1.isChecked()) {
+            Toast.makeText(getApplicationContext(), "체크 했습니다", Toast.LENGTH_SHORT).show();
         }
-        return resultText;
+        else
+            Toast.makeText(getApplicationContext(), "체크 해제 했습니다", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onClick02(View v) {
+
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://m.naver.com"));
+        startActivity(intent);
     }
 
 
-}
+
+    }
+
 
